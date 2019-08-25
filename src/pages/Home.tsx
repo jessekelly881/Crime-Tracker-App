c5cddb3cfd83ea85786efeb767653af2e99cc738
Jesse Kelly <kellyjesse881@gmail.com>
2019-08-25 04:21:00 +0000

    setup mapbox

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import ReactMapboxGl from "react-mapbox-gl";
+import mapboxStyle from './style.json';
+
+const Map = ReactMapboxGl({
+  accessToken: "pk.eyJ1Ijoic29tZWdyYXNzIiwiYSI6ImNqenFwbnJmNTBxb3czbm1pazdwOWo0ZzcifQ.r5yGF8JsDcbaQG-8tv6RRQ"
+});
 
 const Home: React.FunctionComponent = () => {
   return (
@@ -9,8 +15,8 @@ const Home: React.FunctionComponent = () => {
           <IonTitle>CrimeWatch</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">
-          Content
+      <IonContent>
+          <Map containerStyle={{height: "30vh", width: "100%"}} style={mapboxStyle}/>
       </IonContent>
     </>
   );
