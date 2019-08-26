744e82b1d2980fc4d10578d09ed763ce536c1b0b
Jesse Kelly <kellyjesse881@gmail.com>
2019-08-26 17:19:45 +0000

    added working toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonPage, IonRouterOutlet } from '@ionic/react';
+import { IonFab, IonFabButton, IonIcon, IonTabs, IonTabBar, IonTabButton, IonLabel, IonApp, IonPage, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog, faHome, faBiohazard } from '@fortawesome/free-solid-svg-icons';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -23,17 +26,45 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+const ReportFab: React.FunctionComponent = () => (
+    <IonFab style={{marginBottom: "calc(env(safe-area-inset-bottom)/2)"}} vertical="bottom" horizontal="center" slot="fixed">
+        <IonFabButton color="danger">
+          <FontAwesomeIcon icon={faBiohazard} size="2x"/>
+        </IonFabButton>
+    </IonFab>
+)
+
 const App: React.FunctionComponent = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonPage>
-        <IonRouterOutlet>
-          <Route path="/home" component={Home} exact={true} />
-          <Route exact path="/" render={() => <Redirect to="/home" />} />
-        </IonRouterOutlet>
-      </IonPage>
-    </IonReactRouter>
-  </IonApp>
+    <IonApp>
+        <IonReactRouter>
+            <IonPage>
+                <IonTabs>
+                    <IonRouterOutlet>
+                        <Route path="/home" component={Home} exact={true} />
+                        <Route exact path="/" render={() => <Redirect to="/home" />} />
+                    </IonRouterOutlet>
+
+                    <IonTabBar slot="bottom" color="dark">
+                        <IonTabButton tab="home">
+                            <FontAwesomeIcon icon={faHome} size="lg" />
+                            <IonLabel>Home</IonLabel>
+                        </IonTabButton>
+                        <IonTabButton tab="report">
+                        </IonTabButton>
+                        <IonTabButton tab="settings">
+                            <FontAwesomeIcon icon={faCog} size="lg"/>
+                            <IonLabel>Settings</IonLabel>
+                        </IonTabButton>
+                    </IonTabBar>
+
+                </IonTabs>
+
+                <ReportFab />
+
+            </IonPage>
+        </IonReactRouter>
+
+    </IonApp>
 );
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
-import ReactMapboxGl from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import { Marker } from "react-mapbox-gl";
 import mapboxStyle from './style.json';
+
 
 type GeoLocation = [number, number];
 const defaultCenter: GeoLocation = [-99.1332, 19.4326];
@@ -10,15 +12,21 @@ const BaseMap = ReactMapboxGl({
     accessToken: "pk.eyJ1Ijoic29tZWdyYXNzIiwiYSI6ImNqenFwbnJmNTBxb3czbm1pazdwOWo0ZzcifQ.r5yGF8JsDcbaQG-8tv6RRQ",
 });
 
+const TrackerMap: React.FunctionComponent = (props) => {
+    return (
+          <BaseMap center={defaultCenter} containerStyle={{height: "40vh", width: "100%"}} style={mapboxStyle}>
+              <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+                  <Feature coordinates={defaultCenter} />
+              </Layer>
+          </BaseMap>
+    );
+}
+
 const Home: React.FunctionComponent = () => {
   return (
     <>
-      <IonHeader>
-        <IonToolbar style={{height: "30px"}}>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-          <BaseMap center={defaultCenter} containerStyle={{height: "40vh", width: "100%"}} style={mapboxStyle}/>
+      <IonContent class="content">
+          <TrackerMap />
       </IonContent>
     </>
   );
