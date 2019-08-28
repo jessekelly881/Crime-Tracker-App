71c580df19adb140f53505bafea1c17e636c7509
Jesse Kelly <kellyjesse881@gmail.com>
2019-08-28 18:20:13 +0000

    added functional geo tracking

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 import mapboxStyle from '../misc/mapbox/style.json';
+import { Plugins } from '@capacitor/core';
+const { Geolocation } = Plugins;
 
 const mapboxApiKey = "pk.eyJ1Ijoic29tZWdyYXNzIiwiYSI6ImNqenFwbnJmNTBxb3czbm1pazdwOWo0ZzcifQ.r5yGF8JsDcbaQG-8tv6RRQ";
 
 type GeoLocation = [number, number];
-
+const defaultCenter: GeoLocation = [-99.1332, 19.4326];
 
 interface EventsLayerProps{
     points: GeoLocation[];
@@ -15,25 +17,40 @@ const EventsLayer: React.FC<EventsLayerProps> = ({points}) => {
     const paint =  {'circle-radius': 20, 'circle-color': '#aa0000', 'circle-opacity': 0.4}
 
     return (
-        <Layer type="circle" id="marker" paint={paint}>
-            { points.map( (point, i) => {
+        <Layer type="circle" id="eventsLayer" paint={paint}>
+            {
+
+            points.map( (point, i) => {
                 return (<Feature coordinates={point} />);
-            }) }
+            })
+
+            }
         </Layer>
     )
 }
 
-interface CurrentLocationLayerProps{
-    location: GeoLocation;
-}
+const CurrentLocationLayer: React.FC = () => {
+    const [location, setLocation] = useState();
+    const paint =  {'circle-radius': 4, 'circle-color': 'blue', 'circle-opacity': 1}
 
-const CurrentLocationLayer: React.FC<CurrentLocationLayerProps> = (props) => {
-    const paint =  {'circle-radius': 4, 'circle-color': 'white', 'circle-opacity': 1}
-    return (
-        <Layer type="circle" id="marker" paint={paint}>
-            <Feature coordinates={props.location} />
-        </Layer>
-    )
+    const wait = Geolocation.watchPosition({}, (position, err) => {
+        if(position){
+             setLocation([position.coords.longitude, position.coords.latitude]);
+        }
+    })
+
+    if(location){ //Location Known
+        return (
+            <Layer type="circle" id="currentLocationLayer" paint={paint}>
+                <Feature coordinates={location} />
+            </Layer>
+        )
+    }
+
+    else{
+        return (<> </>)
+    }
+
 }
 
 
@@ -51,6 +68,7 @@ const Map: React.FC<MapProps> = (props) => {
 
     return (
           <BaseMap center={props.center} containerStyle={containerStyle} style={mapboxStyle}>
+              <CurrentLocationLayer />
               <EventsLayer points={[props.center]} />
           </BaseMap>
     );
