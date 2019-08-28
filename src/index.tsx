0b2df2491f65cdc0ee9db649cc1685dd79b2fbb6
Jesse Kelly <kellyjesse881@gmail.com>
2019-08-28 13:09:25 +0000

    added ionic pwa elements

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+defineCustomElements(window);
